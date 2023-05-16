79623d6c6108cd6f0336ee223c9900a88bcf4456
AlexBatura1988 <alex.batura88@gmail.com>
2023-05-16 06:47:21 +0300

    adding BasePage

---
+++ src/test/java/pageObjects/BasePage.java
@@ -0,0 +1,40 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+    WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void fillText(WebElement el, String text) {
+        el.clear();
+        sleep(2000);
+        el.sendKeys(text);
+    }
+
+    public void click(WebElement el) {
+        el.click();
+    }
+
+    public String getText(WebElement el) {
+        return el.getText();
+    }
+
+    public void clear(WebElement el) {
+        el.clear();
+    }
+
+    public void sleep(long mills) {
+        try {
+            Thread.sleep(mills);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
