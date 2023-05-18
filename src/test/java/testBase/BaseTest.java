270d0cc23bc053005803a4c483504a1b8444364a
AlexBatura1988 <alex.batura88@gmail.com>
2023-05-19 01:27:21 +0300

    Adding logs to test case-log4j2

--- src/test/java/testBase/BaseTest.java
+++ src/test/java/testBase/BaseTest.java
@@ -2,6 +2,8 @@ package testBase;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -12,9 +14,11 @@ import java.time.Duration;
 public class BaseTest {
 
     public WebDriver driver;
+    public Logger logger;
 
     @BeforeClass
     public void setup() {
+        logger = LogManager.getLogger(this.getClass());
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
--- src/test/java/testCases/TC_001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC_001_AccountRegistrationTest.java
@@ -10,26 +10,35 @@ public class TC_001_AccountRegistrationTest extends BaseTest {
 
     @Test
     void test_account_Registration() {
+        logger.debug("application logs.....");
+        logger.info("******** Starting TC_001_AccountRegistrationTest **********");
+
         try {
 
             HomePage homePage = new HomePage(driver);
             homePage.clickAccount();
+            logger.info("******** Clicked on My account link **********");
             homePage.clickRegister();
-
+            logger.info("******** Clicked on register link **********");
             AccountRegistrationPage regPage = new AccountRegistrationPage(driver);
+            logger.info("******** Providing customer data **********");
             regPage.setFirstName(randomString().toUpperCase());
             regPage.setLastName(randomString().toUpperCase());
             regPage.setEmail(randomString() + "@gmail.com");
             regPage.setPassword(randomAlphaNumeric());
             regPage.setCheckPolicyBtn();
             regPage.clickContinue();
+            logger.info("******** Clicked on continue **********");
             regPage.sleep(2000);
             String actualConfirmMsg = regPage.getConfirmationMsg();
+            logger.info("******** Validating expected message **********");
             String expectedMsg = "Your Account Has Been Created!";
-            Assert.assertEquals(actualConfirmMsg, expectedMsg);
+            Assert.assertEquals(actualConfirmMsg, expectedMsg, "Test failed");
         } catch (Exception e) {
+            logger.error("test failed");
             Assert.fail();
         }
+        logger.info("******** Finished TC_001_AccountRegistrationTest **********");
 
     }
 
